c9f0a3f7df60228c7e46b21f9ce070721a07e7a1
Christopher Venegas <christophervenegas2@gmail.com>
2020-06-23 23:07:12 -0400

    ⚡️ widget divisor x palabra

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
 void main() {
   runApp(new MyApp());
 }
@@ -7,47 +8,51 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to KerryApp',
-      home: RandomWords()
-    );
+    return MaterialApp(title: 'Welcome to KerryApp', home: RandomWords());
   }
 }
 
-class RandomWords extends StatefulWidget{
+class RandomWords extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return RandomWordsState();
   }
-  
 }
 
-class RandomWordsState extends State<RandomWords>{
+class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('KerryApp'),
-        ),
-        body: _buildSuggestions(),
-      );
+      appBar: AppBar(
+        title: Text('KerryApp'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 
   Widget _buildSuggestions() {
     return ListView.builder(
-      itemBuilder: (context, i) {
-        if (i >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
         }
-        return _buildRow(_suggestions[i]);
-      },  
+        return _buildRow(_suggestions[index]);
+      }
     );
   }
 
-  Widget _buildRow(WordPair pair){
+  Widget _buildRow(WordPair pair) {
     return ListTile(
-      title: Text(pair.asPascalCase),
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
     );
   }
 }
